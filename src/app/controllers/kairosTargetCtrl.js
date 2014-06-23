a64019ffe470ca36ff849aae4e711f78a9285e8a
Elordi Gaston <gelordi@truelogicsoftware.com>
2014-06-23 18:42:20 -0300

    =[#UTAH-72 - KairosDb metric integration]

--- src/app/controllers/kairosTargetCtrl.js
+++ src/app/controllers/kairosTargetCtrl.js
@@ -8,110 +8,89 @@ function (angular, _, kbn) {
 
   var module = angular.module('kibana.controllers');
 
+  var seriesList = null;
+
   module.controller('KairosTargetCtrl', function($scope, $timeout) {
 
     $scope.init = function() {
-      $scope.target.errors = validateTarget($scope.target);
-      $scope.aggregators = ['avg', 'sum', 'min', 'max', 'dev', 'zimsum', 'mimmin', 'mimmax'];
 
-      if (!$scope.target.aggregator) {
-	$scope.target.aggregator = 'sum';
+      $scope.aggregators  = [
+	"avg",
+	"dev",
+	"max",
+	"min",
+	"rate",
+	"sort",
+	"sum",
+	"least_squares",
+	"percentile",
+	"scale"
+      ];
+
+      $scope.units        = [
+	"milliseconds",
+	"seconds",
+	"minutes",
+	"hours",
+	"days",
+	"weeks",
+	"months",
+	"years"
+      ];
+
+      if (!$scope.target.aggregators) {
+	$scope.target.aggregators = [];
       }
 
-      if (!$scope.target.downsampleAggregator) {
-	$scope.target.downsampleAggregator = 'sum';
+      if (!$scope.target.groups) {
+	$scope.target.groups = [];
       }
 
-      $scope.$on('typeahead-updated', function() {
-	$timeout($scope.targetBlur);
+      if (!$scope.target.tags) {
+	$scope.target.tags = [];
+      }
+
+      $scope.oldSeries = $scope.target.series;
+      $scope.$on('typeahead-updated', function(){
+	$timeout($scope.get_data);
       });
     };
 
-    $scope.targetBlur = function() {
-      $scope.target.errors = validateTarget($scope.target);
-
-      // this does not work so good
-      if (!_.isEqual($scope.oldTarget, $scope.target) && _.isEmpty($scope.target.errors)) {
-	$scope.oldTarget = angular.copy($scope.target);
+    // Cannot use typeahead and ng-change on blur at the same time
+    $scope.seriesBlur = function() {
+      if ($scope.oldSeries !== $scope.target.series) {
+	$scope.oldSeries = $scope.target.series;
 	$scope.get_data();
       }
     };
 
-    $scope.duplicate = function() {
-      var clone = angular.copy($scope.target);
-      $scope.panel.targets.push(clone);
+    // called outside of digest
+    $scope.listColumns = function(query, callback) {
+      if (!$scope.columnList) {
+	$scope.$apply(function() {
+	  $scope.datasource.listColumns($scope.target.series).then(function(columns) {
+	    $scope.columnList = columns;
+	    callback(columns);
+	  });
+	});
+      }
+      else {
+	return $scope.columnList;
+      }
     };
 
-    $scope.suggestMetrics = function(query, callback) {
-      $scope.datasource
-	.performSuggestQuery(query, 'metrics')
-	.then(callback);
+    $scope.listSeries = function(query, callback) {
+      if (!seriesList) {
+	seriesList = [];
+	$scope.datasource.listSeries().then(function(series) {
+	  seriesList = series;
+	  callback(seriesList);
+	});
+      }
+      else {
+	return seriesList;
+      }
     };
-
-    $scope.suggestTagKeys = function(query, callback) {
-      $scope.datasource
-	.performSuggestQuery(query, 'tagk')
-	.then(callback);
-    };
-
-    $scope.suggestTagValues = function(query, callback) {
-      $scope.datasource
-	.performSuggestQuery(query, 'tagv')
-	.then(callback);
-    };
-
-    $scope.addTag = function() {
-      if (!$scope.addTagMode) {
-	$scope.addTagMode = true;
-	return;
-      }
-
-      if (!$scope.target.tags) {
-	$scope.target.tags = {};
-      }
-
-      $scope.target.errors = validateTarget($scope.target);
-
-      if (!$scope.target.errors.tags) {
-	$scope.target.tags[$scope.target.currentTagKey] = $scope.target.currentTagValue;
-	$scope.target.currentTagKey = '';
-	$scope.target.currentTagValue = '';
-	$scope.targetBlur();
-      }
-
-      $scope.addTagMode = false;
-    };
-
-    $scope.removeTag = function(key) {
-      delete $scope.target.tags[key];
-      $scope.targetBlur();
-    };
-
-    function validateTarget(target) {
-      var errs = {};
-
-      if (!target.metric) {
-	errs.metric = "You must supply a metric name.";
-      }
-
-      if (target.shouldDownsample) {
-	try {
-	  if (target.downsampleInterval) {
-	    kbn.describe_interval(target.downsampleInterval);
-	  } else {
-	    errs.downsampleInterval = "You must supply a downsample interval (e.g. '1m' or '1h').";
-	  }
-	} catch(err) {
-	  errs.downsampleInterval = err.message;
-	}
-      }
-
-      if (target.tags && _.has(target.tags, target.currentTagKey)) {
-	errs.tags = "Duplicate tag key '" + target.currentTagKey + "'.";
-      }
-
-      return errs;
-    }
 
   });
 
--- src/app/services/kairosdb/kairosdbDatasource.js
+++ src/app/services/kairosdb/kairosdbDatasource.js
@@ -12,12 +12,189 @@ function (angular, _, kbn) {
 
     function KairosDatasource(datasource) {
       this.type = 'kairosdb';
-      this.editorSrc = 'app/partials/kairosdb/editor.html';
+      this.basicAuth = datasource.basicAuth;
       this.url = datasource.url;
+      this.username = datasource.username;
+      this.password = datasource.password;
+      this.editorSrc = 'app/partials/kairosdb/editor.html';
       this.name = datasource.name;
+
+      this.templateSettings = {
+	interpolate : /\[\[([\s\S]+?)\]\]/g,
+      };
     }
 
+    KairosDatasource.prototype.query = function(filterSrv, options) {
+      //console.log("ds query");
+      //console.log(filterSrv);
+      //console.log(options);
+
+      var payload = {};
+
+      this.translateTime(options.range.from, payload, "start");
+      this.translateTime(options.range.to, payload, "end");
+
+      var delta = this.deltaTime(options.range.from, options.range.to);
+      var value = delta / options.maxDataPoints;
+
+      payload.metrics = _.chain(options.targets).reject(function(target) {
+	return (!target.series /*|| !target.column*/ || target.hide);
+      }).map(function(target) {
+
+	//console.log("QUERY!!!! ",target);
+
+	var obj = {};
+	obj.name = target.series;
+	obj.tags = {};
+	obj.aggregators = [{name: "avg", sampling:{value: value, unit: "milliseconds"}}];
+	return obj;
+      })
+      .value();
+
+      //console.log("Payload2", payload);
+
+      var query = {
+	method: 'POST',
+	url: '/api/v1/datapoints/query',
+	data: payload
+	// headers: {
+	//   'Content-Type': 'application/x-www-form-urlencoded',
+	// }
+      };
+      return this.doKairosDBRequest(query).then(handleKairosDBQueryResponse);
+    }
+
+    KairosDatasource.prototype.listSeries = function() {
+      return this.doKairosDBRequest({url:'/api/v1/metricnames'}).then(function(results) {
+	if (!results.data) {
+	  return [];
+	}
+
+	return results.data.results
+      });
+    };
+
+    KairosDatasource.prototype.doKairosDBRequest = function(options) {
+      if (!options.method) {
+	options.method = 'GET';
+      }
+
+      options.url = this.url + options.url;
+
+      return $http(options);
+    };
+
+    function handleKairosDBQueryResponse(results) {
+      var output = [];
+
+      _.each(results.data.queries, function(series) {
+	var sample_size = series.sample_size;
+
+	_.each(series.results, function(result, index) {
+
+	  var target = result.name;
+	  var datapoints = [];
+
+	  for(var i = 0; i < result.values.length; i++) {
+	    var t = Math.floor(result.values[i][0] / 1000);
+	    var v = result.values[i][1];
+	    datapoints[i] = [v,t];
+	  }
+
+	  output.push({ target:target, datapoints:datapoints });
+	});
+      });
+
+      var output2 = { data: _.flatten(output) };
+
+      return output2;
+    }
+
+    KairosDatasource.prototype.translateTime = function(date, response_obj, start_stop_name) {
+      if (_.isString(date)) {
+	if (date === 'now') {
+	  return;
+	}
+	else if (date.indexOf('now-') >= 0) {
+	  name = start_stop_name + "_relative";
+
+	  date = date.substring(4);
+	  var re_date = /(\d+)\s*(\D+)/;
+	  var result = re_date.exec(date);
+	  if (result) {
+	    var value = result[1];
+	    var unit = result[2];
+	    switch(unit) {
+	      case 'ms':
+		unit = 'milliseconds';
+		break;
+	      case 's':
+		unit = 'seconds';
+		break;
+	      case 'm':
+		unit = 'minutes';
+		break;
+	      case 'h':
+		unit = 'hours';
+		break;
+	      case 'd':
+		unit = 'days';
+		break;
+	      case 'w':
+		unit = 'weeks';
+		break;
+	      case 'M':
+		unit = 'months';
+		break;
+	      case 'y':
+		unit = 'years';
+		break;
+	      default:
+		break;
+	    }
+	    response_obj[name] = {
+	      "value": value,
+	      "unit": unit
+	    };
+	    return;
+	  }
+	  return;
+	}
+
+	date = kbn.parseDate(date);
+      }
+      name = start_stop_name + "_absolute";
+
+      date = moment.utc(date);
+
+      if (dashboard.current.timezone === 'browser') {
+	date = date.local();
+      }
+
+      if (config.timezoneOffset) {
+	date = date.zone(config.timezoneOffset);
+      }
+
+      response_obj[name] = date.valueOf();
+    };
+
+    KairosDatasource.prototype.datetime = function(kibana_datetime) {
+
+      var date = kbn.parseDate(kibana_datetime);
+      date = moment.utc(date);
+      return date;
+    };
+
+    KairosDatasource.prototype.deltaTime = function(start, end) {
+      var startdate = this.datetime(start);
+      var enddate = this.datetime(end);
+
+      var delta = enddate.diff(startdate, 'milliseconds');
+      return delta;
+    };
+
     return KairosDatasource;
+
   });
 
 });

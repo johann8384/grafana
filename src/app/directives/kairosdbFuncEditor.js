c817231082f0a0bdcdce60df611d24a64eb0f17e
Elordi Gaston <gelordi@truelogicsoftware.com>
2014-06-25 18:02:02 -0300

    =[#UTAH-80 - KairosDb querying tags]

--- src/app/directives/kairosdbFuncEditor.js
+++ src/app/directives/kairosdbFuncEditor.js
@@ -18,6 +18,38 @@ function (angular, _, $) {
 	}
       };
 
+    })
+    .directive('kairosGroupby', function($compile) {
+
+      return {
+	restrict: 'E',
+	scope: true,
+	templateUrl: 'app/partials/kairosdb/groups.html',
+	link: function(scope, elem) {
+
+	  scope.listGroups = [];
+
+	  scope.newTag = function() {
+	    return {name:'tag',tags:[]}
+	  }
+
+	  scope.newTime = function() {
+	    return {name:'time',group_count:'',range_size:{value:'',unit:''}}
+	  }
+
+	  scope.newValue = function() {
+	    return {name:'value',range_size:''}
+	  }
+
+	  scope.$watch('newType',function(){
+	    if (angular.isDefined(scope.newType)) {
+	       scope.listGroups.push(scope.newType.value);
+	    }
+	  });
+
+	}
+      };
+
     });
 
 });

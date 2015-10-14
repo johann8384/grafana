cfbe3bdefa40ae2e7340f1e41011337ea52f07d6
Jonathan Creasy <jonathan@ghostlab.net>
2015-10-14 13:37:44 -0500

    Working Docker container

--- src/config.sample.js
+++ src/config.sample.js
@@ -1,111 +1,70 @@
 // == Configuration
 // config.js is where you will find the core Grafana configuration. This file contains parameter that
 // must be set before Grafana is run for the first time.
-
-define(['settings'], function(Settings) {
-  "use strict";
+define(['settings'],
+function (Settings) {
 
   return new Settings({
 
-      /* Data sources
-      * ========================================================
-      * Datasources are used to fetch metrics, annotations, and serve as dashboard storage
-      *  - You can have multiple of the same type.
-      *  - grafanaDB: true    marks it for use for dashboard storage
-      *  - default: true      marks the datasource as the default metric source (if you have multiple)
-      *  - basic authentication: use url syntax http://username:password@domain:port
-      */
-
-      // InfluxDB example setup (the InfluxDB databases specified need to exist)
-      /*
-      datasources: {
-        influxdb: {
-          type: 'influxdb',
-          url: "http://my_influxdb_server:8086/db/database_name",
-          username: 'admin',
-          password: 'admin',
-        },
-        grafana: {
-          type: 'influxdb',
-          url: "http://my_influxdb_server:8086/db/grafana",
-          username: 'admin',
-          password: 'admin',
-          grafanaDB: true
-        },
+    // datasources, you can add multiple
+    datasources: {
+      elasticsearch: {
+        grafanaDB: true,
+        index: 'grafana-dash',
+        type: 'elasticsearch',
+        url: 'http://dashboard.ops.turn.com/search',
       },
-      */
-
-      // Graphite & Elasticsearch example setup
-      /*
-      datasources: {
-        graphite: {
-          type: 'graphite',
-          url: "http://my.graphite.server.com:8080",
-        },
-        elasticsearch: {
-          type: 'elasticsearch',
-          url: "http://my.elastic.server.com:9200",
-          index: 'grafana-dash',
-          grafanaDB: true,
-        }
+      graphite: {
+        type: 'graphite',
+        url: 'http://ops016.sjc2.turn.com:80',
       },
-      */
-
-      // OpenTSDB & Elasticsearch example setup
-      /*
-      datasources: {
-        opentsdb: {
-          type: 'opentsdb',
-          url: "http://opentsdb.server:4242",
-        },
-        elasticsearch: {
-          type: 'elasticsearch',
-          url: "http://my.elastic.server.com:9200",
-          index: 'grafana-dash',
-          grafanaDB: true,
-        }
+      opentsdb: {
+        default: true,
+        type: 'opentsdb',
+        url: 'http://dwh-edge003.atl1.turn.com:4245',
       },
-      */
-
-      /* Global configuration options
-      * ========================================================
-      */
-
-      // specify the limit for dashboard search results
-      search: {
-        max_results: 100
+      opentsdbM: {
+        type: 'opentsdbM',
+        url: 'http://dashboard.ops.turn.com:4242',
       },
+    },
 
-      // default home dashboard
-      default_route: '/dashboard/file/default.json',
+    /* Global configuration options
+    * ========================================================
+    */
 
-      // set to false to disable unsaved changes warning
-      unsaved_changes_warning: true,
+    // specify the limit for dashboard search results
+    search: {
+      max_results: 20
+    },
 
-      // set the default timespan for the playlist feature
-      // Example: "1m", "1h"
-      playlist_timespan: "1m",
+    // default start dashboard
+    default_route: '/dashboard/file/default.json',
 
-      // If you want to specify password before saving, please specify it below
-      // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
-      admin: {
-        password: ''
-      },
+    // set to false to disable unsaved changes warning
+    unsaved_changes_warning: true,
 
-      // Change window title prefix from 'Grafana - <dashboard title>'
-      window_title_prefix: 'Grafana - ',
+    // set the default timespan for the playlist feature
+    // Example: "1m", "1h"
+    playlist_timespan: "1m",
 
-      // Add your own custom panels
-      plugins: {
-        // list of plugin panels
-        panels: [],
-        // requirejs modules in plugins folder that should be loaded
-        // for example custom datasources
-        dependencies: [],
-      }
+    // If you want to specify password before saving, please specify it bellow
+    // The purpose of this password is not security, but to stop some users from accidentally changing dashboards
+    admin: {
+      password: ''
+    },
 
-    });
+    // Change window title prefix from 'Grafana - <dashboard title>'
+    window_title_prefix: 'Grafana - ',
+
+    // Add your own custom panels
+    plugins: {
+      // list of plugin panels
+      panels: [],
+      // requirejs modules in plugins folder that should be loaded
+      // for example custom datasources
+      dependencies: [],
+    }
+
+  });
 });
-
-
-
